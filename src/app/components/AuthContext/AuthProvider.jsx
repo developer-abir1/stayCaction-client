016921f605a8f70df67c454505d30c4bccfc6518
developer abir <developerabir40@gmail.com>
2023-02-13 22:28:12 +0600

    client

--- src/app/components/AuthContext/AuthProvider.jsx
+++ src/app/components/AuthContext/AuthProvider.jsx
@@ -2,14 +2,31 @@ import React from 'react';
 import { useState } from 'react';
 import { createContext } from 'react';
 import app from '../../../../firebaseConfig';
-console.log(app);
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from 'firebase/auth';
+
 export const AuthContext = createContext();
 const AuthProvider = ({ children }) => {
   const [bookingInfo, setBookingInfo] = useState({});
+  const [loading, setLoading] = useState(true);
+  const auth = getAuth();
+
+  const createAnAccount = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  const singInAccount = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password);
+  };
 
   const data = {
     bookingInfo,
     setBookingInfo,
+    createAnAccount,
+    singInAccount,
   };
 
   return <AuthContext.Provider value={data}>{children}</AuthContext.Provider>;
